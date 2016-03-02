d7ee183cb7b8e0267b3eafe58fcfe0f57533fa62
Ilya Shupta <funn17@gmail.com>
2016-03-02 13:10:40 +0600

    Basic item.

--- gustoqa/items.py
+++ gustoqa/items.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Item, Field
 
 
-class GustoqaItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class GustoqaItem(Item):
+    name = Field()
+    url = Field()
+    ingredients = Field()
+    recipeYield = Field()
+    prepTime = Field()
+    cookTime = Field()
+    recipeInstructions = Field()
+    image_urls = Field()
+    images = Field()
